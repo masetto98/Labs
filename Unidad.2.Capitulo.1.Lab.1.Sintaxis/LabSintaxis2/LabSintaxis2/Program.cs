8a301c996b3f01400e31558c19052c407a0f26d1
Santiago <smasetto@hotmail.com>
2021-04-15 13:15:58 -0300

    Complete 1.Estructuras de decisión

--- Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis2/LabSintaxis2/Program.cs
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis2/LabSintaxis2/Program.cs
@@ -6,7 +6,34 @@ namespace LabSintaxis2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Introducir texto");
+            string inputTexto;        
+            inputTexto = Console.ReadLine();
+            if (inputTexto != "")
+            {
+                Console.WriteLine("Menu de opciones" + "\n Ingrese alguna de las siguientes opciones" + "\n 1. Mayuscula" + "\n 2. Minuscula" + "\n 3. Longitud");
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                if (opcion.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("Longitud del texto ingresado:" + inputTexto.Length);
+                }
+                else
+                {
+                    if (opcion.Key == ConsoleKey.D1)
+                    {
+                        Console.WriteLine("Conversión:" + inputTexto.ToUpper());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Conversion:" + inputTexto.ToLower());
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se ha ingresado texto");
+
+            }
         }
     }
 }
