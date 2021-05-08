75e6ec6387a38f8eedc0db8b4c3eaba2427a3ffc
Santiago <smasetto@hotmail.com>
2021-05-08 13:29:49 -0300

    Complete lab LINQ

---
+++ Unidad.2.Extra.Lab.3.LINQ/LabsLINQ-2.3.1/LabsLINQ-2.3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabsLINQ_2._3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string[] provincias;
+            // provincias = new string[23] { "Jujuy", "Salta", "Formosa", "Chaco", "Corrientes", "Misiones", "Catamarca", "Tucuman", "Santiago del Estero", "Santa Fe", "Entre Rios", "La Rioja", "Cordoba", "San Juan", "San Luis", "Mendoza", "La Pampa", "Buenos Aires", "Neuquen", "Rio Negro", "Chubut", "Santa Cruz", "Tierra del Fuego" };
+            var provincias = new List<string> { "Jujuy", "Salta", "Formosa", "Chaco", "Corrientes", "Misiones", "Catamarca", "Tucuman", "Santiago del Estero", "Santa Fe", "Entre Rios", "La Rioja", "Cordoba", "San Juan", "San Luis", "Mendoza", "La Pampa", "Buenos Aires", "Neuquen", "Rio Negro", "Chubut", "Santa Cruz", "Tierra del Fuego" };
+            var misProv = from p in provincias where (p.StartsWith("S") || p.StartsWith("T")) select p;
+            foreach (var p in misProv)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
---
+++ Unidad.2.Extra.Lab.3.LINQ/LabsLINQ2.3.3/LabsLINQ2.3.3/Ciudades.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsLINQ2._3._3
+{
+    class Ciudades
+    {
+        private string _nombre;
+        private int _codpostal;
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+        public int CodPostal
+        {
+            get
+            {
+                return _codpostal;
+            }
+            set
+            {
+                _codpostal = value;
+            }
+        }
+    
+    }
+    
+}
