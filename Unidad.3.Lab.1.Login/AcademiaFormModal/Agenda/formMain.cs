d472a08d8a8a19fcf6d09f763cfe980c3752482a
Santiago <smasetto@hotmail.com>
2021-07-08 12:31:45 -0300

    Complete Lab Unid 3 L1 y L2

---
+++ Unidad.3.Lab.1.Login/AcademiaFormModal/Agenda/formMain.cs
@@ -0,0 +1,42 @@
+﻿using AcademiaFormModal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuSalir_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void formMain_Shown(object sender, EventArgs e)
+        {
+            formLogin appLogin = new formLogin();
+            if (appLogin.ShowDialog() != DialogResult.OK)
+            {
+                this.Dispose();
+            }
+
+
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
