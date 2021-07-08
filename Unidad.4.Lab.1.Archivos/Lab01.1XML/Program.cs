9e72e59d533f180f757197e5edc0f1f0b98c416f
Santiago <smasetto@hotmail.com>
2021-07-08 12:20:07 -0300

    Complete Lab Unid 4.L1(Archivos)

---
+++ Unidad.4.Lab.1.Archivos/Lab01.1XML/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Lab01._1XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Presione una tecla para generar el archivo agendaxml.xml con los datos de agenda.txt");
+            Console.ReadKey();
+            escribirXML();
+            Console.WriteLine("Archivo agendaxml.xml generado correctamente\n\nPresione una tecla para ver su contenido");
+            Console.ReadKey();
+            Console.WriteLine();
+            leerXML();
+            Console.ReadKey();
+        }
+        private static void escribirXML()
+        {
+            XmlTextWriter escritorXML = new XmlTextWriter("agendaxml.xml", null);
+            escritorXML.Formatting = Formatting.Indented;
+            escritorXML.WriteStartDocument(true);
+            escritorXML.WriteStartElement("DocumentElement");
+            StreamReader lector = File.OpenText("agenda.txt");
+            string linea;
+            do
+            {
+                linea = lector.ReadLine();
+                if (linea != null)
+                {
+                    string[] valores = linea.Split(";");
+                    escritorXML.WriteStartElement("contactos");
+                    escritorXML.WriteStartElement("nombre");
+                    escritorXML.WriteValue(valores[0]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteStartElement("apellido");
+                    escritorXML.WriteValue(valores[1]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteStartElement("apellido");
+                    escritorXML.WriteValue(valores[2]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteStartElement("telefono");
+                    escritorXML.WriteValue(valores[3]);
+                    escritorXML.WriteEndElement();
+                    escritorXML.WriteEndElement();
+                }
+            }
+            while (linea != null);
+            escritorXML.WriteEndElement();
+            escritorXML.WriteEndDocument();
+            escritorXML.Close();
+
+            lector.Close();
+        }
+        private static void leerXML()
+        {
+            XmlTextReader lectorXML = new XmlTextReader("agendaxml.xml");
+            string tagAnterior = "";
+            while(lectorXML.Read())
+            {
+                if (lectorXML.NodeType == XmlNodeType.Element)
+                {
+                    tagAnterior = lectorXML.Name;
+                }
+                else if(lectorXML.NodeType == XmlNodeType.Text)
+                {
+                    Console.WriteLine(tagAnterior + ": " + lectorXML.Value);
+                }
+            }
+            lectorXML.Close();
+        }
+    }
+}
