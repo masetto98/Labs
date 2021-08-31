908585283acae494973f189fb0300bbe6a1592f6
Santiago <smasetto@hotmail.com>
2021-08-31 18:31:45 -0300

    Complete Lab Unid 4 L4

---
+++ Unidad4.Lab4-ADO.Net-MySQL/Unid4-Lab4-Ej1/Unid4-Lab4-Ej1/ContactosMysqlConDataAdapter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Unid4_Lab4_Ej1
+{
+    class ContactosMysqlConDataAdapter : Contactos
+    {
+        protected string connectionString
+        {
+            get
+            {
+                return "server=localhost;database=net;uid=root;pwd=root;";
+
+            }
+        }
+        protected MySqlDataAdapter adapter;
+        public ContactosMysqlConDataAdapter(): base()
+        {
+            this.adapter.InsertCommand = new MySqlCommand(
+                "insert into contactos values(@id,@nombre,@apellido,@email,@telefono)");
+            this.adapter.InsertCommand.Parameters.Add("@id", MySqlDbType.Int32, 1, "id");
+            this.adapter.InsertCommand.Parameters.Add("@nombre", MySqlDbType.VarChar, 20, "nombre");
+            this.adapter.InsertCommand.Parameters.Add("@apellido", MySqlDbType.VarChar, 20, "apellido");
+            this.adapter.InsertCommand.Parameters.Add("@email", MySqlDbType.VarChar, 50, "email");
+            this.adapter.InsertCommand.Parameters.Add("@telefono", MySqlDbType.VarChar, 10, "telefono");
+
+            this.adapter.UpdateCommand = new MySqlCommand(
+                "update contactos set nombre=@nombre, apellido=@apellido, email=@email,telefono=@telefono " +
+                " where id=@id");
+            this.adapter.UpdateCommand.Parameters.Add("@id", MySqlDbType.Int32, 1, "id");
+            this.adapter.UpdateCommand.Parameters.Add("@nombre", MySqlDbType.VarChar, 20, "nombre");
+            this.adapter.UpdateCommand.Parameters.Add("@apellido", MySqlDbType.VarChar, 20, "apellido");
+            this.adapter.UpdateCommand.Parameters.Add("@email", MySqlDbType.VarChar, 50, "email");
+            this.adapter.UpdateCommand.Parameters.Add("@telefono", MySqlDbType.VarChar, 10, "telefono");
+
+            this.adapter.DeleteCommand = new MySqlCommand("delete from contactos where id=@id");
+            this.adapter.DeleteCommand.Parameters.Add("@id", MySqlDbType.Int32, 1, "id");
+        }
+        public override DataTable getTabla()
+        {
+            this.adapter = new MySqlDataAdapter("select * from contactos", this.connectionString);
+            DataTable contactos = new DataTable();
+            this.adapter.Fill(contactos);
+            return contactos;
+        }
+        public override void aplicaCambios()
+        {
+            using (MySqlConnection Conn = new MySqlConnection(this.connectionString))
+            {
+                this.adapter.InsertCommand.Connection = Conn;
+                this.adapter.UpdateCommand.Connection = Conn;
+                this.adapter.DeleteCommand.Connection = Conn;
+                this.adapter.Update(this.misContactos);
+            }
+        }
+
+
+    }
+}
---
+++ Unidad4.Lab4-ADO.Net-MySQL/Unid4-Lab4-Ej1/Unid4-Lab4-Ej1/ManejadorArchivoXML.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Unid4_Lab4_Ej1
+{
+    class ManejadorArchivoXML: ManejadorArchivo
+    {
+        protected DataSet ds;
+        public override System.Data.DataTable getTabla()
+        {
+            this.ds = new DataSet();
+            this.ds.ReadXml("agenda.xml");
+            return this.ds.Tables["contactos"];
+        }
+        public override void aplicaCambios()
+        {
+            this.ds.WriteXml("agenda.xml");
+            this.ds.WriteXml("agendaconschemaxml", XmlWriteMode.WriteSchema);
+        }
+    }
+}
